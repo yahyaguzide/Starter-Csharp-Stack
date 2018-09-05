7b140884fc96f70b75e0991d532ee09e3c8de766
yahya guzide <yahyaguzide@hotmail.de>
2018-09-05 18:37:49 +0200

    Added Stack Class

--- Starter-Csharp-Stack/Program.cs
+++ Starter-Csharp-Stack/Program.cs
@@ -11,6 +11,29 @@ namespace Starter_Csharp_Stack
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to my Little Stack Simulator \n Please choose a Type");
+            string type = Console.ReadLine();
+            switch(type)
+            {
+                case "int":
+                case "Int":
+                    Stack<int> stack = new Stack<int>();
+                    break;
+
+                case "char":
+                case "Char":
+                    Stack<char> stack = new Stack<char>();
+                    break;
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Options \n P for Push \n F for Flush \n G for Get \n");
+            string option = Console.ReadLine();
+
+            
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Starter-Csharp-Stack/Stack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Starter_Csharp_Stack
+{
+    class Stack<T>
+    {
+        // List's makes this program quit easy
+        List<T> list = new List<T>();
+       
+       void Push(T obj)
+        {
+            list.Add(obj);
+        }
+        void Flush()
+        {
+            list.RemoveRange(0, list.Count);
+        }
+        T Get(int index)
+        {
+            return list[index];
+        }
+       
+    }
+}
